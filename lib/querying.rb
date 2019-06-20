340e3e40223d2563b46bd6c949eefe26a8405c27
Jared Grove <jaredkgrove@gmail.com>
2019-06-20 22:08:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,7 +47,7 @@ def select_series_title_with_most_human_characters
   ON characters.series_id = series.id
   WHERE characters.species = 'human'
   GROUP BY series.title
-  ORDER BY 
+  ORDER BY COUNT(series.title)
 
   "
   #
