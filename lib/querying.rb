5499d297eb719e421fa6958d601c7f1d3cb68fe6
Jared Grove <jaredkgrove@gmail.com>
2019-06-20 22:07:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,7 @@ end
 
 def select_series_title_with_most_human_characters
   "
-  SELECT series.title, COUNT(series.title)
+  SELECT series.title
   FROM characters
   INNER JOIN series
   ON characters.series_id = series.id
