9d5c14d967dc7e10c05e5e0b3af7594a852845b5
Jared Grove <jaredkgrove@gmail.com>
2019-06-20 21:31:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,6 +23,7 @@ def select_value_and_count_of_most_prolific_species
   SELECT characters.species, COUNT(characters.species)
   FROM characters
   GROUP BY characters.species
+  ORDER BY COUNT(characters.species)
   "
 end
 
