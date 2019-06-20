491f7c7084c3d5be182b3d537d2d35c0b28dc19f
Jared Grove <jaredkgrove@gmail.com>
2019-06-20 21:58:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,7 +45,7 @@ def select_series_title_with_most_human_characters
   FROM characters
   INNER JOIN series
   ON characters.series_id = series.id
-
+  WHERE characters.species = '*'
   GROUP BY series.title
   "
 end
