9742842d869d80a8471d921f4462724ed7ae0d1e
Jared Grove <jaredkgrove@gmail.com>
2019-06-20 20:58:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -13,6 +13,14 @@ INSERT INTO authors (name) VALUES
 INSERT INTO books (title, year, series_id) VALUES
 ("book 1.1", 1990, 1),
 ("book 2.1", 2000, 1),
+("book 3.1", 2010, 1),
+("book 1.2", 1991, 2),
+("book 2.2", 2001, 2),
+("book 3.2", 2011, 2);
+
+INSERT INTO books (title, year, series_id) VALUES
+("book 1.1", 1990, 1),
+("book 2.1", 2000, 1),
 ("book 3.1", 2010, 1);,
 ("book 1.2", 1991, 2),
 ("book 2.2", 2001, 2),
