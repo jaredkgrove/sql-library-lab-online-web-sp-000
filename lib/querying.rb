8907aca972e71dd4e19196b47febbf2763416342
Jared Grove <jaredkgrove@gmail.com>
2019-06-20 22:03:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,7 @@ end
 
 def select_series_title_with_most_human_characters
   "
-  SELECT series.title, characters.species
+  SELECT MAX(series.title)
   FROM characters
   INNER JOIN series
   ON characters.series_id = series.id
