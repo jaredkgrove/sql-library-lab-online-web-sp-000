25eb18d5d1b2a5271b7aa6f1d188b97e3c236ff2
Jared Grove <jaredkgrove@gmail.com>
2019-06-20 20:44:38 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,19 +1,23 @@
 CREATE TABLE characters (
-
+  id INTEGER PRIMARY KEY,
+  
 )
 
 CREATE TABLE books (
-
+id INTEGER PRIMARY KEY,
 )
 
 CREATE TABLE series (
-
-)
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  sub_genre_id INTEGER
+);
 
 CREATE TABLE authors (
-
+id INTEGER PRIMARY KEY,
 )
 
 CREATE TABLE sub_genres (
-
+id INTEGER PRIMARY KEY,
 )
